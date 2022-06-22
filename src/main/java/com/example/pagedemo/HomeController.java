8c73891c108dc5a20f6425fa9076212eb220a621
KimHaYeon <khy920723@gmail.com>
2022-06-22 22:10:14 +0900

    20220622_HomeController_fixing

--- src/main/java/com/example/pagedemo/HomeController.java
+++ src/main/java/com/example/pagedemo/HomeController.java
@@ -1,4 +1,23 @@
 package com.example.pagedemo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("/pagedemo")
+    public String jspPage(Model model){
+//        model.addAttribute("name", "springboot1234");
+
+        return "home";
+    }
 }
